fbd742308215adec3e23fc2cc96a75b9c7cb3190
=eyal <=JB53Jb53@gmail.com>
2020-02-14 12:32:15 +0200

    get current time on button click

--- 2-class-ex-client.js
+++ 2-class-ex-client.js
@@ -1,1 +1,18 @@
-console.log('hello!');
+const ELEMENTS = {
+    $getCurrentTimeBtn: $('#get-current-time-btn'),
+    $addTimestampBtn: $('add-timestamp-btn'),
+    $getHistoryBtn: $('#get-history-btn'),
+    $response: $('#response'),
+}
+
+main();
+
+function main() {
+    ELEMENTS.$getCurrentTimeBtn.on('click', () => {
+        $.ajax('http://localhost:5000/time', {
+            success: (data) => {
+                ELEMENTS.$response.html(data);
+            },
+        });
+    });
+}
