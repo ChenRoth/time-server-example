257e3c91c3ffd9cdca58c89c16932c2747de8386
=eyal <=JB53Jb53@gmail.com>
2020-02-14 12:38:41 +0200

    get history and add timestamps on button clicks

--- 2-class-ex-client.js
+++ 2-class-ex-client.js
@@ -1,6 +1,6 @@
 const ELEMENTS = {
     $getCurrentTimeBtn: $('#get-current-time-btn'),
-    $addTimestampBtn: $('add-timestamp-btn'),
+    $addTimestampBtn: $('#add-timestamp-btn'),
     $getHistoryBtn: $('#get-history-btn'),
     $response: $('#response'),
 }
@@ -10,6 +10,23 @@ main();
 function main() {
     ELEMENTS.$getCurrentTimeBtn.on('click', () => {
         $.ajax('http://localhost:5000/time', {
+            success: (data) => {
+                ELEMENTS.$response.html(data);
+            },
+        });
+    });
+
+    ELEMENTS.$addTimestampBtn.on('click', () => {
+        $.ajax('http://localhost:5000/time', {
+            method: 'POST',
+            success: (data) => {
+                ELEMENTS.$response.html(data);
+            },
+        });
+    });
+
+    ELEMENTS.$getHistoryBtn.on('click', () => {
+        $.ajax('http://localhost:5000/history', {
             success: (data) => {
                 ELEMENTS.$response.html(data);
             },
